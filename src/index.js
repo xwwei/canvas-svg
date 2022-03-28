f32f372b9af9b0480a75c6b1fa82e233023139ab
wei.zhang <wei.zhang@ivydad.com>
2022-03-28 17:53:43 +0800

    fix ug

--- src/index.js
+++ src/index.js
@@ -13,34 +13,23 @@ function animate() {
 
 }
 
-requestAnimationFrame( animate )
+requestAnimationFrame(animate)
 
 // 500个svg标签
-// let  svg = `<svg class="lds-message" width="80px" height="80px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-// <g transform="translate(20 50)">
-//     <circle cx="0" cy="0" r="7" fill="#e15b64" transform="scale(0.99275 0.99275)">
-//         <animateTransform attributeName="transform" type="scale" begin="-0.375s" calcMode="spline" keySplines="0.3 0 0.7 10.3 0 0.7 1" values="010" keyTimes="00.51" dur="1s" repeatCount="indefinite"></animateTransform>
-//     </circle>
-// </g>
-// <g transform="translate(40 50)">
-//     <circle cx="0" cy="0" r="7" fill="#f47e60" transform="scale(0.773605 0.773605)">
-//         <animateTransform attributeName="transform" type="scale" begin="-0.25s" calcMode="spline" keySplines="0.3 0 0.7 10.3 0 0.7 1" values="010" keyTimes="00.51" dur="1s" repeatCount="indefinite"></animateTransform>
-//     </circle>
-// </g>
-// <g transform="translate(60 50)">
-//     <circle cx="0" cy="0" r="7" fill="#f8b26a" transform="scale(0.42525 0.42525)">
-//         <animateTransform attributeName="transform" type="scale" begin="-0.125s" calcMode="spline" keySplines="0.3 0 0.7 10.3 0 0.7 1" values="010" keyTimes="00.51" dur="1s" repeatCount="indefinite"></animateTransform>
-//     </circle>
-// </g>
-// <g transform="translate(80 50)">
-//     <circle cx="0" cy="0" r="7" fill="#abbd81" transform="scale(0.113418 0.113418)">
-//         <animateTransform attributeName="transform" type="scale" begin="0s" calcMode="spline" keySplines="0.3 0 0.7 10.3 0 0.7 1" values="010" keyTimes="00.51" dur="1s" repeatCount="indefinite"></animateTransform>
-//     </circle>
-// </g>
+// let  svg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30px" height="30px" viewBox="0 0 24 24" style="enable-background:new 0 0 50 50" xml:space="preserve">
+// <rect x="0" y="0" width="4" height="7" fill="#FF6700" transform="scale(1 1.94336)">
+//     <animateTransform attributeType="xml" attributeName="transform" type="scale" values="1,1; 1,3; 1,1" begin="0s" dur="0.6s" repeatCount="indefinite"></animateTransform>
+// </rect>
+// <rect x="10" y="0" width="4" height="7" fill="#FF6700" transform="scale(1 2.72331)">
+//     <animateTransform attributeType="xml" attributeName="transform" type="scale" values="1,1; 1,3; 1,1" begin="0.2s" dur="0.6s" repeatCount="indefinite"></animateTransform>
+// </rect>
+// <rect x="20" y="0" width="4" height="7" fill="#FF6700" transform="scale(1 1.38997)">
+//     <animateTransform attributeType="xml" attributeName="transform" type="scale" values="1,1; 1,3; 1,1" begin="0.4s" dur="0.6s" repeatCount="indefinite"></animateTransform>
+// </rect>
 // </svg>`
 
 // let svgs = ''
-// for (let i = 0; i < 500; i++) {
+// for (let i = 0; i < 1000; i++) {
 //   svgs += svg
 // }
 
